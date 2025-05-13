bd487f618fba98a85cd71a12528da8c03abcc816
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-13 16:16:03 -0400

    updated reducer example

--- src/App.jsx
+++ src/App.jsx
@@ -34,15 +34,26 @@ function App() {
   return (
     <div style={{ margin: 5, padding: 10 }}>
       <div style={{ display: "flex", gap: 15 }}>
-        <button style={{ padding: 20, marginBottom: 15, marginTop: 15 }}>
+        <button
+          style={{ padding: 20, marginBottom: 15, marginTop: 15 }}
+          onClick={() => dispatch({ type: "increment" })}
+        >
           +
         </button>
         <h1>Count: {state.count}</h1>
-        <button style={{ padding: 20, marginBottom: 15, marginTop: 15 }}>
+        <button
+          style={{ padding: 20, marginBottom: 15, marginTop: 15 }}
+          onClick={() => dispatch({ type: "decrement" })}
+        >
           -
         </button>
       </div>
-      <button style={{ marginLeft: 100 }}>Reset</button>
+      <button
+        style={{ marginLeft: 100 }}
+        onClick={() => dispatch({ type: "reset" })}
+      >
+        Reset
+      </button>
       {/* <UserProvider>
         <UserProfile />
         <UpdateUser />
