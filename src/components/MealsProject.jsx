04963f02078b3266b761c8657d7a35d3ca2bdb33
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-13 21:28:33 -0400

    added MealsProject

--- src/components/MealsProject.jsx
+++ src/components/MealsProject.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 import "../index.css";
 
 function MealsProject() {
-  return <div>MealsProject</div>;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        setItems(res.data.meals);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const itemslist = items.map(({ strMeal, strMealThumb, idMeal }) => {
+    return (
+      <section className="card">
+        <img src={strMealThumb} />
+        <section className="content">
+          <p>{strMeal}</p>
+          <p>#{idMeal}</p>
+        </section>
+      </section>
+    );
+  });
+
+  return <div className="items-container">{itemslist}</div>;
 }
 
 export default MealsProject;
